3cbdaf07033c098d5fb0ed96c73992364c354779
DaniBotz <108689250+YTDANIBOTZ@users.noreply.github.com>
2022-07-19 21:04:31 +0800

    Update dani.js

--- dani.js
+++ dani.js
@@ -2799,7 +2799,7 @@ Bot Ini Adalah Beta Multi-Device WhatsApp.
 𖥂 ${prefix}.vote _text_
 𖥂 ${prefix}.devote
 𖥂 ${prefix}.upvote
-𖥂 ${prefix.}cekvote
+𖥂 ${prefix}.cekvote
 𖥂 ${prefix}.hapusvote
 └───────⭓
 
